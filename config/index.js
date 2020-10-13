11b354d0fbfceb1f60f93ec5213c3e7ae13e4e2d
Gledyson <gledysonferreira@gmail.com>
2020-10-13 17:42:03 -0400

    renamed env variable process.env.port to process.env.PORT

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  port: process.env.port || 8000,
-  host: process.env.host || "127.0.0.1",
+  port: process.env.PORT || 8000,
+  host: process.env.HOST || "127.0.0.1",
   baseUrl: process.env.BASE_URL || "http://127.0.0.1:8000",
   clientUrl: process.env.CLIENT_URL || "http://127.0.0.1:3000",
   fromEmailAddress: process.env.FROM_EMAIL_ADDRESS,
