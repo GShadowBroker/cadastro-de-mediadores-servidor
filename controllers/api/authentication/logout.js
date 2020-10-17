417a07babfe1d9fe24bca759e6183cde9266227d
Gledyson <gledysonferreira@gmail.com>
2020-10-16 20:58:27 -0400

    updated cookie description under logout endpoint

--- controllers/api/authentication/logout.js
+++ controllers/api/authentication/logout.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   return res
-    .clearCookie("sid", { httpOnly: true })
+    .clearCookie("sid", {
+      httpOnly: true,
+      maxAge: authExpiration,
+      secure: process.env.NODE_ENV === "production",
+    })
     .status(200)
     .json({ success: true });
 });
